34688007c6c7379fda93b91a115b326671375f42
boguckiw <boguckiw@student.mini.pw.edu.pl>
2021-05-15 11:33:39 +0200

    Reading data script

---
+++ read_data.R
@@ -0,0 +1,20 @@
+library(funModeling)
+
+
+digits_train <- read.table("data/digits_train.data")
+digits_train_labels <- read.table("data/digits_train.labels")
+
+head(digits_train)
+status_dig <- status(digits_train)
+View(status_dig)
+# remove columns with only one unique value
+one_unique <- which(status_dig$unique==1)
+digits_train <- digits_train[,-one_unique]
+
+
+artif_train <- read.table("data/artificial_train.data")
+artif_train_labels <- read.table("data/artificial_train.labels")
+
+status_art <- status(artif_train)
+View(status_art)
+which(status_art$unique==1)
